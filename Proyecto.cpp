2aa469b30188f065070ce98d288b67c51872b9d7
AlfonsoCBTEC <A01714310@tec.mx>
2025-11-16 16:17:59 -0600

    Seleccion_proyecto.cpp

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -1,3 +1,16 @@
+/*
+Proyecto por Alfonso Carranza Bassoco - A01714310
+Este proyecto es un sistema de gestión de biblioteca 
+desarrollado en C++ con enfoque en programación orientada a objetos. 
+El objetivo es modelar y administrar elementos como libros, usuarios 
+y préstamos, integrando conceptos fundamentales de POO: clases, atributos,
+métodos, herencia, composición y diagramas UML.
+
+El sistema permite registrar libros, gestionar usuarios y controlar préstamos
+activos, además de servir como práctica para representar y analizar relaciones
+entre objetos mediante diagramas de clase y de secuencia
+*/
+
 #include<iostream>
 
 using namespace std;
