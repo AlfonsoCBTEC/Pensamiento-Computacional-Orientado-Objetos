46d4879f880b425abb68cdda86f7b9ac8f3e711d
AlfonsoCBTEC <A01714310@tec.mx>
2025-11-16 23:33:17 -0600

    Avance1 Proyecto.cpp

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -11,11 +11,32 @@ activos, además de servir como práctica para representar y analizar relaciones
 entre objetos mediante diagramas de clase y de secuencia
 */
 
-#include<iostream>
+#include <iostream>
+#include "Libro.cpp"
 
 using namespace std;
 
-int main(){
-  cout<<"Bienvenido al Proyecto";
-  return 0;
+int main() {
+
+    cout << "Bienvenido al sistema de gestión de biblioteca de Alfonso\n\n";
+
+    // Primer objeto lo cree usando el constructor vacio
+    Libro libro1;
+    libro1.set_autor("J.K. Rowling");
+    libro1.set_nombre("Harry Potter y la piedra filosofal");
+    libro1.set_paginas(223);
+    libro1.set_isbn(123456);
+
+    // Para crear este objeto use el constructor con parametros
+    Libro libro2("George Orwell", 328, "1984", 987654);
+
+    cout << "=== Libro 1 ===" << endl;
+    libro1.mostrar_info();
+    cout << endl;
+
+    cout << "=== Libro 2 ===" << endl;
+    libro2.mostrar_info();
+    cout << endl;
+
+    return 0;
 }
