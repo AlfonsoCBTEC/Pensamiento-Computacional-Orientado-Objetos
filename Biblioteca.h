a9a171df919d8e12a9d3192403d167c89675dbb3
AlfonsoCBTEC <A01714310@tec.mx>
2025-11-23 21:33:53 -0600

    Update Biblioteca.h
    -Agregue el metodo de agregar un usuario
    -Agregue metodo para mostrar el catalogo de librios

--- Biblioteca.h
+++ Biblioteca.h
@@ -41,16 +41,42 @@ class Biblioteca{
             } else {
                 cout << "La biblioteca esta llena. No se pueden agregar mas libros" << endl;
             }
-
         }
 
+        void agregar_usuario(Usuario _usuario){
+            if(total_usuarios < 20){
+                usuarios[total_usuarios] = _usuario;
+                total_usuarios ++;
 
+                 cout << "Tu libro usuario fue registrado en la biblioteca!" << endl;
+                 cout << "Total de Usuarios: " << total_libros << endl;
 
+            } else {
+                cout << "El sistema esta lleno. No se pueden agregar mas usuarios" << endl;
+            }   
+        }
+
+        void mostrar_catalogo(){
+
+            cout << "\n=======================================" << endl;
+            cout << "         CATALOGO DE A BIBLIOTECA        " << endl;
+            cout << "\n=======================================" << endl;
+
+            if(total_libros == 0){
+                cout << "No hay libros registrados en la biblioteca" << endl;
+                return;
+            }
+
+            for(int i=0; i<total_libros; i++){
+                cout << "--- Libro #" << i + 1 << "---" << endl;
+                libros[i].mostrar_info();
+            }
+
+            cout << "-----------------------------------------" << endl;
+            cout << "Total de libros en el catalogo: "<< total_libros << endl;
+        }
 
 };
-
-
-
 
 
 #endif
