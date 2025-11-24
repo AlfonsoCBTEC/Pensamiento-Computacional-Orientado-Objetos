1be64d9bb755d24ef8452b872655b6a9c9811fe6
AlfonsoCBTEC <A01714310@tec.mx>
2025-11-23 20:40:48 -0600

    Create Usuario.h

---
+++ Usuario.h
@@ -0,0 +1,65 @@
+#ifndef USUARIO
+#define USUARIO
+
+#include <iostream>
+
+using namespace std;
+
+class Usuario{
+
+    private:
+        string nombre;
+        int idUsuario;
+        string contraseña;
+    
+
+    public:
+
+        Usuario() {}
+
+        Usuario(string _nombre, int _idUsuario, string _contraseña){
+            nombre = _nombre;
+            idUsuario = _idUsuario;
+            contraseña = _contraseña;
+        }
+
+        //Getters
+
+        string get_nombre(){
+            return nombre;
+        }
+
+        int get_idUsuario(){
+            return idUsuario;
+        }
+
+        string get_contraseña(){
+            return contraseña;
+        }
+
+        //Setters
+
+        void set_nombre(string _nombre){
+            nombre = _nombre;
+        }
+
+        
+        void set_idUsuario(int _idUsuario){
+            idUsuario = _idUsuario;
+        }
+
+        
+        void set_contraseña(string _contraseña){
+            contraseña = _contraseña;
+        }
+
+        //metodos
+
+        void mostrar_usuario(){
+            cout << "Nombre: " << nombre << endl;
+            cout << "ID: " << idUsuario << endl;
+        }
+
+};
+
+#endif
