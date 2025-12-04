2046e3c92dd5f8480f034b85e45cd1dce53322f6
AlfonsoCBTEC <A01714310@tec.mx>
2025-12-03 23:53:09 -0600

    Update Usuario.h

--- Usuario.h
+++ Usuario.h
@@ -1,5 +1,16 @@
-#ifndef USUARIO
-#define USUARIO
+/*
+ * Proyecto Biblioteca
+ * Alfonso Carranza Bassoco
+ * A01714310
+ */
+
+/*
+ * Clase Usuario, contiene metodo para mostrar
+ *la informacion del usuario a execpcion de su contrasena
+ */
+
+#ifndef USUARIO_H_
+#define USUARIO_H_
 
 #include <iostream>
 
@@ -8,15 +19,16 @@ using namespace std;
 class Usuario{
 
     private:
+        //Declaracion de atributos
         string nombre;
         int idUsuario;
         string contraseña;
     
 
     public:
-
+        //Constructor vacio
         Usuario() {}
-
+        //Constructor con parametros
         Usuario(string _nombre, int _idUsuario, string _contraseña){
             nombre = _nombre;
             idUsuario = _idUsuario;
@@ -24,33 +36,30 @@ class Usuario{
         }
 
         //Getters
-
-        string get_nombre(){
+        string getNombre(){
             return nombre;
         }
 
-        int get_idUsuario(){
+        int getIdUsuario(){
             return idUsuario;
         }
 
-
         //Setters
-
-        void set_nombre(string _nombre){
+        void setNombre(string _nombre){
             nombre = _nombre;
         }
 
         
-        void set_idUsuario(int _idUsuario){
+        void setIdUsuario(int _idUsuario){
             idUsuario = _idUsuario;
         }
 
         
-        void set_contraseña(string _contraseña){
+        void setContraseña(string _contraseña){
             contraseña = _contraseña;
         }
 
-        //metodos
+        //Metodos
 
         void mostrar_usuario(){
             cout << "Nombre: " << nombre << endl;
@@ -59,4 +68,4 @@ class Usuario{
 
 };
 
-#endif
+#endif // USUARIO_H_
