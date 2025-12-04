e5608e6282ee64bfabe4eec2330a3087dddf0001
AlfonsoCBTEC <A01714310@tec.mx>
2025-12-03 23:51:42 -0600

    Update Material.h

--- Material.h
+++ Material.h
@@ -1,45 +1,61 @@
-#ifndef MATERIAL
-#define MATERIAL
+/*
+ * Proyecto Biblioteca
+ * Alfonso Carranza Bassoco
+ * A01714310
+ */
+
+/*
+ * Clase Material, clase padre de la cual derivan Libro y Revista.
+ */
+
+
+#ifndef MATERIAL_H_
+#define MATERIAL_H_
 
 #include <iostream>
 
 using namespace std;
 
 class Material{
+
     
     protected:
+        //Declara atributos que van a tener el comun los materiales de la biblioteca
         string titulo;
         string autor;
 
     public:
-
+        //Constructor por default
         Material() {}
-
+        //Constructor con parametros
         Material(string _titulo, string _autor){
             titulo = _titulo;
             autor = _autor;
         }
 
-        string get_titulo(){
+        //Getters
+        string getTitulo(){
             return titulo;
         }
 
-        string get_autor(){
+        string getAutor(){
             return autor;
         }
-
-        void set_titulo(string _titulo){
+        
+        //Setters
+        void setTitulo(string _titulo){
             titulo = _titulo;
         }
 
-        void set_autor(string _autor){
+        void setAutor(string _autor){
             autor = _autor;
         }
 
+        //Metodos
         void mostrar_info_basica(){
             cout << "Titulo: " << titulo << ", Autor: " << autor << endl;
         }
 
 };
 
-#endif
+#endif // MATERIAL_H_
