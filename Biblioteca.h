eaf2812ce779b02c0f63f1f371691450c21c8557
AlfonsoCBTEC <A01714310@tec.mx>
2025-12-03 23:51:12 -0600

    Update Biblioteca.h

--- Biblioteca.h
+++ Biblioteca.h
@@ -1,11 +1,24 @@
-#ifndef BIBLIOTECA
-#define BIBLIOTECA
+/*
+ * Proyecto Biblioteca
+ * Alfonso Carranza Bassoco
+ * A01714310
+ */
 
+/*
+ * Clase Biblioteca. Clase principal, maneja y almacena en listas
+ * los usuarios registrados y el material de la biblioteca
+ * divido en libros y revistas.
+ */
+
+#ifndef BIBLIOTECA_H_
+#define BIBLIOTECA_H_
+
+#include "Prestamo.h"
 #include "Libro.h"
 #include "Usuario.h"
 #include "Revista.h"
 #include <iostream>
-
+#include <vector>
 
 using namespace std;
 
@@ -13,6 +26,7 @@ using namespace std;
 class Biblioteca{
 
     private:
+        //Atrubutos. Listas que van a almacenar los objetos.
         Libro libros[100];
         int total_libros;
 
@@ -22,19 +36,25 @@ class Biblioteca{
         Revista revistas[50];
         int total_revistas;
 
+        vector<Prestamo> historialPrestamos; // Ya vimos vectores en clase, ahora lo utilizo en vez de arrays
+
         string nombre;
 
     public:
 
-        //incicializar biblioteca
+        //Constructor por default, incializa la biblioteca.
         Biblioteca (){
             total_libros = 0;
             total_usuarios = 0;
             total_revistas = 0;
             nombre = "Biblioteca de Alfonso";
         }
-        
 
+        /*
+         * Agrega los objetos tipo libro al arreglo de libros.
+         * Si el total de libros definido por el arrgelo excede
+         * no lo agrega y muestra un cout
+        */
         void agregar_libro(Libro _libro){
             if (total_libros < 100){
                 libros[total_libros] = _libro;
@@ -47,7 +67,12 @@ class Biblioteca{
                 cout << "La biblioteca esta llena. No se pueden agregar mas libros" << endl;
             }
         }
-
+        
+        /*
+         * Agrega los objetos tipo usuario al arreglo de usuarios.
+         * Si el total de usuarios definido por el arrgelo excede
+         * no lo agrega y muestra un cout
+        */
         void agregar_usuario(Usuario _usuario){
             if(total_usuarios < 20){
                 usuarios[total_usuarios] = _usuario;
@@ -60,7 +85,12 @@ class Biblioteca{
                 cout << "El sistema esta lleno. No se pueden agregar mas usuarios" << endl;
             }   
         }
-        
+
+         /*
+         * Agrega los objetos tipo revista al arreglo de revistas.
+         * Si el total de revistas definido por el arrgelo excede
+         * no la agrega y muestra un cout
+        */
         void agregar_revista(Revista _revista){
             if(total_revistas < 50){
                 revistas[total_revistas] = _revista;
@@ -75,6 +105,17 @@ class Biblioteca{
             }
         }
 
+        void agregarPrestamo(){
+            
+
+        }
+
+        /*
+         *Utliza el arreglo de libros y revistas y su indice.
+         *Recorre ambos arreglos e imprime la infomacion de cada 
+         *objeto. Si no hay objetos en el arreglo no entra el for 
+         *y se muestra un cout.
+        */
         void mostrar_catalogo(){
 
             cout << "\n=======================================" << endl;
@@ -108,6 +149,12 @@ class Biblioteca{
 
         }
 
+        /*
+         *Utliza el arreglo de usuarios su indice.
+         *Recorre el arreglo e imprime la infomacion de cada 
+         *objeto. Si no hay objetos en el arreglo no entra el for 
+         *y se muestra un cout.
+        */
         void mostrar_usuarios(){
             cout << "\n=======================================" << endl;
             cout << "         USUARIOS DE LA BIBLIOTECA        " << endl;
@@ -130,4 +177,4 @@ class Biblioteca{
 };
 
 
-#endif
+#endif // BIBLIOTECA_H_
